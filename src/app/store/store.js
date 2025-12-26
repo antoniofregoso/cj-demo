c8bc25506975f9fd6ed3934f1a469cf7935bd33d
antoniofregoso <antoniofregoso@protonmail.com>
2025-12-26 13:39:36 -0600

    [FIX] A non-serializable value was detected in an action, in the path:  error in persistedReducer

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { persistStore, persistReducer} from 'redux-persist';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import contextSlice from "./slices/contextSlice";
 import homeSlice from "./slices/homeSlice";
@@ -8,30 +8,36 @@ import byeSlice from "./slices/byeSlice";
  * Configure the Redux store with slices and persistence.
  */
 const persistConfig = {
-    key: 'root',
-    storage
-  };
+  key: 'root',
+  storage
+};
 /**
  * Combine the slices into a root reducer.
  */
-  const rootReducer = combineReducers({
-    context: contextSlice,
-    home: homeSlice,
-    bye: byeSlice
-  });
+const rootReducer = combineReducers({
+  context: contextSlice,
+  home: homeSlice,
+  bye: byeSlice
+});
 /**
  * Create a persisted reducer using the root reducer and persistence configuration.
  */
-  const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 /**
  * Configure the Redux store with the persisted reducer.
  */
-  const store = configureStore({
-    reducer: persistedReducer
-  });
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 /**
  * Create a persistor to manage the persistence of the store.
  */
-  const persistor = persistStore(store); 
+const persistor = persistStore(store);
 
-  export { store, persistor };
+export { store, persistor };
